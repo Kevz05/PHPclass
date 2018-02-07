e1b2cf199c70141885a2e5be087b475c017beb78
Kevin Chesbro <200008676@fvtc.edu>
2018-02-07 17:55:01 -0600

    Start of PHP class.

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset = "utf-8"/>
+        <title>PHP Class - Kevin Chesbro</title>
+        <link rel="stylesheet" type="text/css" href="css/mystyles.css"/>
+    </head>
+
+    <body>
+        <div class = "page-wrap">
+            <header class="site-header">
+                <h1>PHP Class - Kevin Chesbro</h1>
+            </header>
+            <div class="flex-box">
+            <nav class = "main-nav">
+                <ul>
+                    <li class = "menu-item"><a href="/">Home</a></li>
+                    <li class = "menu-item"><a href="#">Loops</a></li>
+                    <li class = "menu-item"><a href="#">Strings</a></li>
+                </ul>
+            </nav>
+
+            <section class="main-content">
+
+                <!-- <div class="img">picture</div> -->
+                <h2>Hello!</h2>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, nulla non vehicula rutrum, neque nibh varius nisi, nec ultricies tortor ligula eu est. Aenean vel feugiat quam. Aliquam erat volutpat. Pellentesque hendrerit rutrum nisi at blandit. Nulla condimentum nulla vel libero posuere fermentum. Vestibulum ligula lectus, sagittis sit amet massa non, sagittis tempor lacus. Suspendisse luctus arcu finibus condimentum consectetur. Ut malesuada vel velit ac fringilla. Mauris mi velit, elementum ut vulputate feugiat, posuere rhoncus tortor. Quisque eget tellus nibh. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla semper mi eu nisl suscipit, et pellentesque lacus suscipit. Vestibulum posuere iaculis metus, eu venenatis nisi consectetur convallis. Donec quis gravida dui.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, nulla non vehicula rutrum, neque nibh varius nisi, nec ultricies tortor ligula eu est. Aenean vel feugiat quam. Aliquam erat volutpat. Pellentesque hendrerit rutrum nisi at blandit. Nulla condimentum nulla vel libero posuere fermentum. Vestibulum ligula lectus, sagittis sit amet massa non, sagittis tempor lacus. Suspendisse luctus arcu finibus condimentum consectetur. Ut malesuada vel velit ac fringilla. Mauris mi velit, elementum ut vulputate feugiat, posuere rhoncus tortor. Quisque eget tellus nibh. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla semper mi eu nisl suscipit, et pellentesque lacus suscipit. Vestibulum posuere iaculis metus, eu venenatis nisi consectetur convallis. Donec quis gravida dui.</p>
+                </div>
+            </section>
+
+            <aside class="right-sidebar">Sidebar</aside>
+
+            </div> <!-- flex box ends -->
+
+
+            <footer>
+                <div>Learn PHP <sup>&copy;</sup> 2018</div>
+            </footer>
+
+
+        </div> <!-- /page-wrap -->
+    </body>
+
+</html>
